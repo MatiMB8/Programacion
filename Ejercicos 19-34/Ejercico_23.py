d925f72fc28b6b77e8a275d9dc6a85c106e8a7c1
MatiMB <101474153+MatiMB8@users.noreply.github.com>
2024-10-18 19:54:02 +0200

    Ejercico_23.py

--- Ejercicos 19-34/Ejercico_23.py
+++ Ejercicos 19-34/Ejercico_23.py
@@ -2,18 +2,15 @@
 #notable (>=6.5 -<8.5), satisfactorio (<6.5 -5) o insuficiente (<5). Controla que la nota 
 #introducida esté entre 0 y 10. Utilizar elif sin operadores lógicos.
 
+var = float(input("Introduce tu nota: "))
 
-var=float(input("Introduce tu nota: "))
-
-
-if var<5:
-        print("Has sacado un ",var,", tu nota es insuficiente.")
-elif var>=5 and var<6.5:
-       print("Has sacado un",var,", tu nota es satisfactoria.")
-elif var>=6.5 and var<8.5:
-        print("Has sacado un ",var,", tu nota es notable.")
-elif var>=8.5 and var<=10.:
-        print("Has sacado un ",var,", tu nota es excelente.")
+if var < 5:
+    print("Has sacado un ", var, ", tu nota es insuficiente.")
+elif 5 <= var < 6.5:
+    print("Has sacado un ", var, ", tu nota es satisfactoria.")
+elif 6.5 <= var < 8.5:
+    print("Has sacado un ", var, ", tu nota es notable.")
+elif 8.5 <= var <= 10:
+    print("Has sacado un ", var, ", tu nota es excelente.")
 else:
-    print("La nota que has introducido no esta entre el 0 y el 10.")
-        
+    print("La nota que has introducido no está entre el 0 y el 10.")
