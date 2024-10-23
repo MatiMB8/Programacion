ad36f383b7c671c248db61e1a694f55ff2446f17
MatiMB <101474153+MatiMB8@users.noreply.github.com>
2024-10-23 13:37:06 +0200

    Add files via upload

---
+++ Ejercicos 19-34/Ejercicio_33.py
@@ -0,0 +1,11 @@
+#33. Programa un código que permita contar el número de vocales de la siguiente frase: No 
+#hay mal que dure cien años
+
+var="No hay mal que dure cien años"
+var1=var.count("a")
+var2=var.count("e")
+var3=var.count("i")
+var4=var.count("o")
+var5=var.count("u")
+
+print("El número de a es ",var1," el número de e es ",var2," el número de i es ",var3," el número de o es ",var4," y el número de u es ",var5)
